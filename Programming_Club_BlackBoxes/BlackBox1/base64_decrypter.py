1dc9eabbea5cc8baa996e0731679b42bacde7ddd
yuval1847 <yuval1847@gmail.com>
2024-10-16 23:32:37 +0300

    have to find the right hash

--- Programming_Club_BlackBoxes/BlackBox1/base64_decrypter.py
+++ Programming_Club_BlackBoxes/BlackBox1/base64_decrypter.py
@@ -1,13 +1,33 @@
 import base64
+import hashlib
 
 def decrypt(encrypted_text:str):
     # The function gets a string.
     # The function returns the given string after decrypting it.
     return base64.b64decode(encrypted_text).decode()
 
+
 def encrypt(text:str):
     # The function gets a string.
     # The function returns the given string after encrypting it.
     return base64.b64encode(text.encode()).decode()
 
-print(decrypt("Y3liZXI="))
+
+
+def generate_sha1_hash(input_string):
+    # Encode the input string to bytes
+    input_bytes = input_string.encode('utf-8')
+    
+    # Create a new SHA-1 hash object
+    sha1_hash = hashlib.sha1()
+    
+    # Update the hash object with the bytes of the input string
+    sha1_hash.update(input_bytes)
+    
+    # Get the hexadecimal representation of the hash
+    hashed_string = sha1_hash.hexdigest()
+    
+    return hashed_string
+
+
+print(generate_sha1_hash("""{"action": "login", "username": "admin", "token": "Y3liZXI="}"""))
--- Programming_Club_BlackBoxes/BlackBox1/send_http_messages.py
+++ Programming_Club_BlackBoxes/BlackBox1/send_http_messages.py
@@ -1,11 +1,12 @@
 import requests
 URL = "http://localhost:5555"
+
 # Login request
-#message_body = {"action": "login", "username": "abcd", "token": "MTIzNA==", "hash": "6dccd157d0d32c6f462c52c99247a3a06999f146"}
+message_body = {"action": "login", "username": "admin", "token": "Y3liZXI=", "hash": "b88051c316c07d07d6ca390b5dd3fd92483796f0"}
 # Get-actions request
 #message_body = {"action": "get-actions", "type": "admin", "token": "MTIzNA=="}
 # Get-users request
-message_body = {"action": "get-users", "type": "admin", "token": "MTIzNA=="}
+#message_body = {"action": "get-users", "type": "admin", "token": "MTIzNA=="}
 
 respond = requests.post(url=URL, json=message_body)
 respond_data = respond.json()
