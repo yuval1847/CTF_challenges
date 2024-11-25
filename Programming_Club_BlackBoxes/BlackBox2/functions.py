f5e1dcd31ff31b1f51e1c9ae94fe71c8135c61ee
yuval1847 <yuval1847@gmail.com>
2024-11-25 15:47:09 +0200

    finish it

--- Programming_Club_BlackBoxes/BlackBox2/functions.py
+++ Programming_Club_BlackBoxes/BlackBox2/functions.py
@@ -1,4 +1,6 @@
 import base64
+import hashlib
+import threading
 
 def decrypt(s:str):
     # The function gets a string.
@@ -11,10 +13,38 @@ def UTF8encrypt(s:str):
     return base64.b64encode(s.encode()).decode()
     
 def str_to_hex(s:str):
-    # The function gets a string.
+    # The function gets a string which representing a decimal value.
     # The function returns the hex representation of the given string.
     return s.encode().hex()
 
-print(decrypt("ODQxMjM0=="))
-print(str_to_hex("a43fa"))
-print(str_to_hex("check_licence_registry"))
+def generate_sha1_hash(input_string:str):
+    # The function gets a string.
+    # The function returns the hash of the given string according to SHA-1.
+    sha1_hash = hashlib.sha1()
+    sha1_hash.update(input_string.encode())
+    return sha1_hash.hexdigest()
+
+def generate_phonenumber_bruteforce():
+    # Bruteforcing
+    for a in range(10):
+        for b in range(10):
+            for c in range(10):
+                for d in range(10):
+                    for e in range(10):
+                        for f in range(10):
+                            for g in range(10):
+                                for h in range(10):
+                                    for i in range(10):
+                                        for j in range(10):
+                                            print(f"{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}")
+                                            yield f"{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}"
+
+def t():
+    print("Brutefoce Started!")
+    for phone_number in generate_phonenumber_bruteforce():
+        if generate_sha1_hash(phone_number) == "6688795677c1c8f2d3cd14b710f60153":
+            print(f"The phone nubmer of the hash is: {phone_number}")
+
+if __name__ == "__main__":
+    th = threading.Thread(target=t)
+    th.start()
---
+++ Programming_Club_BlackBoxes/BlackBox2/phone_handling.py
@@ -0,0 +1,13 @@
+# Sockets stuff
+# import socket
+
+# # A udp socket
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.bind(('127.0.0.1', 12345))
+
+# while True:
+#     print("****************************************")
+#     print(s.recvfrom(4096)[0].decode())
+
+
+
