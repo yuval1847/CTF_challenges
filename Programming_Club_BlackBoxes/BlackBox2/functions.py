67ed8556e6dc66ad6156af9daeb960f6126fb2a4
yuval1847 <yuval1847@gmail.com>
2024-11-26 21:42:27 +0200

    finished blackbox2

--- Programming_Club_BlackBoxes/BlackBox2/functions.py
+++ Programming_Club_BlackBoxes/BlackBox2/functions.py
@@ -17,10 +17,19 @@ def str_to_hex(s:str):
     # The function returns the hex representation of the given string.
     return s.encode().hex()
 
+#*****************************************************************************
+
 def generate_sha1_hash(input_string:str):
     # The function gets a string.
     # The function returns the hash of the given string according to SHA-1.
     sha1_hash = hashlib.sha1()
+    sha1_hash.update(input_string.encode())
+    return sha1_hash.hexdigest()
+    
+def generate_md5_hash(input_string:str):
+    # The function gets a string.
+    # The function returns the hash of the given string according to SHA-1.
+    sha1_hash = hashlib.md5()
     sha1_hash.update(input_string.encode())
     return sha1_hash.hexdigest()
 
@@ -30,21 +39,16 @@ def generate_phonenumber_bruteforce():
         for b in range(10):
             for c in range(10):
                 for d in range(10):
-                    for e in range(10):
-                        for f in range(10):
-                            for g in range(10):
-                                for h in range(10):
-                                    for i in range(10):
-                                        for j in range(10):
-                                            print(f"{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}")
-                                            yield f"{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}"
+                    print(f"{a}{b}{c}{d}")
+                    yield "052538" + f"{a}{b}{c}{d}"
 
-def t():
-    print("Brutefoce Started!")
-    for phone_number in generate_phonenumber_bruteforce():
-        if generate_sha1_hash(phone_number) == "6688795677c1c8f2d3cd14b710f60153":
-            print(f"The phone nubmer of the hash is: {phone_number}")
+# print("Bruteforce Started!")
+# for phone_number in generate_phonenumber_bruteforce():
+#     if generate_sha1_hash(phone_number) == "6688795677c1c8f2d3cd14b710f60153":
+#         print(f"The phone number of the hash is: {phone_number}. The correct hash is: sha-1")
+#         break
+#     elif generate_md5_hash(phone_number) == "6688795677c1c8f2d3cd14b710f60153":
+#         print(f"The phone number of the hash is: {phone_number}. The correct hash is: md5")
+#         break
 
-if __name__ == "__main__":
-    th = threading.Thread(target=t)
-    th.start()
+print(decrypt("bWdkZjMyZ2Y0M0AAAAAAAA==").encode())
--- Programming_Club_BlackBoxes/BlackBox2/phone_handling.py
+++ Programming_Club_BlackBoxes/BlackBox2/phone_handling.py
@@ -1,7 +1,7 @@
 # Sockets stuff
-# import socket
+import socket
 
-# # A udp socket
+# A UDP socket
 # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # s.bind(('127.0.0.1', 12345))
 
@@ -10,4 +10,10 @@
 #     print(s.recvfrom(4096)[0].decode())
 
 
-
+# A TCP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 8543))
+s.send("1234".encode())
+while True:
+    print("****************************************")
+    print(s.recv(4096).decode())
