87bca3912d879841a8e3b1b88ac1a4063c55b1b1
yuval1847 <yuval1847@gmail.com>
2024-11-30 14:51:41 +0200

    BlackBox 4 finished!!!

--- Programming_Club_BlackBoxes/BlackBox4/functions.py
+++ Programming_Club_BlackBoxes/BlackBox4/functions.py
@@ -1,9 +1,17 @@
-import base64
+# import base64
 
-def decrypt(s:str):
-    # The function gets a string.
-    # The function returns the given string after getting decrypted.
-    return base64.b64decode(s.encode()).decode()
+# def decrypt(s:str):
+#     # The function gets a string.
+#     # The function returns the given string after getting decrypted.
+#     return base64.b64decode(s.encode()).decode()
 
-if __name__ == "__main__":
-    print(decrypt("aHR0cDovLzEyNy4wLjAuMTo1MDAwL2ZpbmFsLXN0ZXA="))
+# if __name__ == "__main__":
+#     print(decrypt("aHR0cDovLzEyNy4wLjAuMTo1MDAwL2ZpbmFsLXN0ZXA="))
+
+import socket
+
+HOST, PORT = "127.0.0.1", 8753
+
+# A tcp socket:
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
